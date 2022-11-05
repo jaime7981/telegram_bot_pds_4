cbac661bb14070ee017729313c166cada0c01173
jaime7981 <jmfernandez1@miuandes.cl>
2022-11-05 03:00:46 -0300

    app models registration for admin page

--- telegram_bot/bot_api/admin.py
+++ telegram_bot/bot_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from bot_api.models import Player, Chat, Stats, NumberGame
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Chat)
+admin.site.register(Stats)
+admin.site.register(NumberGame)

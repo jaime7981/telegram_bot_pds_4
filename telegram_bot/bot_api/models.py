e3c7e32dc73f2ca5117cac1e4c42534a4246c1a0
jaime7981 <jmfernandez1@miuandes.cl>
2022-11-09 21:50:57 -0300

    updated model for trivia game

--- telegram_bot/bot_api/models.py
+++ telegram_bot/bot_api/models.py
@@ -27,9 +27,39 @@ class NumberGame(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     game_state = models.CharField(default='W', max_length=1, choices=GAME_STATES)
     attempts = models.IntegerField(default=0, null=True, blank=True)
-    response = models.IntegerField(default=None, null=True, blank=True)
-    won = models.BooleanField(default=False, null=True, blank=True)
 
     rule_attempts = models.IntegerField(default=5, null=True, blank=True)
     rule_highest = models.IntegerField(default=100, null=True, blank=True)
     answer = models.IntegerField(default=None, null=True, blank=True)
+
+
+class Question(models.Model):
+    ans1 = models.CharField(max_length=100)
+    ans2 = models.CharField(max_length=100)
+    ans3 = models.CharField(max_length=100)
+    ans4 = models.CharField(max_length=100)
+    correct = models.CharField(max_length=100)
+
+class TriviaGame(models.Model):
+    GAME_STATES = (
+        ('W', 'Write'),
+        ('B', 'Block')
+    )
+
+    GAME_MODES = (
+        ('F', 'First'),
+        ('T', 'Time')
+    )
+
+    question = models.ForeignKey(Question, default=None)
+
+    game_state = models.CharField(default='W', max_length=1, choices=GAME_STATES)
+    game_mode = models.CharField(default='F', max_length=1, choices=GAME_MODES)
+    num_of_questions = models.IntegerField(default='1')
+
+class TriviaGameInstance(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    player = models.ForeignKey(TriviaGame, on_delete=models.CASCADE)
+
+    points = models.IntegerField(default=0)
